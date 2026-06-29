4a432a3694e05f32eacda0a7b772222c5b89f4c7
Vinayak2006 <varmavinayak2006@gmail.com>
2026-06-29 14:21:48 +0530

    Calculating LCM using GCD

---
+++ LCM.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std ;
+int LCM(int num){
+
+    int a[num] ;
+    int i , j ;
+    for (i = 0 ; i < num ; i++)
+    {
+        cout<<"Enter the "<<i+1<<" number :"<<endl ;
+        cin>>a[i] ;
+    }
+    
+}
