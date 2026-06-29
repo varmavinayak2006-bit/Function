674df2bab48b13a46709b992c206a67cc0992fc5
Vinayak2006 <varmavinayak2006@gmail.com>
2026-06-29 13:27:01 +0530

    Lottery distribution

---
+++ lottery.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std ;
+void  money(int amount)
+{
+    int A,B,C,D;
+    // amount already with the members are as follows
+    A = 15 ;
+    B = 10 ;
+    C = 5  ;
+    D = 20 ;
+    cout<<"The lottery ticket amonut :" <<endl ;
+    int fees = A + B + C + D ;
+    cout<<fees ;
+    cout<<"The winning pool prize is :"<<amount ;
+    // calculating the proportionality 
+    
+
+
+}
